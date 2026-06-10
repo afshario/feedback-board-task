f1496e6c0e64973659d20624e4f8246475e6a979
MHAfshari <mhafshari1385@gmail.com>
2026-06-10 20:56:51 +0330

    feat(db): add SQLAlchemy database setup with session management

---
+++ src/db.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+from typing import Generator
+from config import DB_URL
+
+# create engine
+engine = create_engine(
+      DB_URL,
+      pool_size=10,          
+      max_overflow=20,
+      pool_pre_ping=True  
+)
+
+# create SessionLocal
+SessionLocal = sessionmaker(
+      autocommit=False,
+      autoflush=False,
+      bind=engine
+)
+
+# Base class for models
+Base = declarative_base()
+
+
+# get_db dependency for endpoints
+def get_db() -> Generator[Session]:
+      '''
+      create session for each request
+      '''
+      db = SessionLocal()
+      try:
+            yield db
+      finally:
+            db.close()
