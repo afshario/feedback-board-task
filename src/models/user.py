74e36ce8cf57d5a29afd835be19a0b966b505bd5
MHAfshari <mhafshari1385@gmail.com>
2026-06-10 21:29:54 +0330

    feat(models): add User ORM model

---
+++ src/models/user.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
+import bcrypt
+from sqlalchemy.sql import func
+from db import Base
+
+class User(Base):
+      __tablename__ = "users"
+    
+      id = Column(Integer, primary_key=True, index=True)
+      username = Column(String(50), unique=True, nullable=False, index=True)
+      password = Column(String(255), nullable=False)
+      is_admin = Column(Boolean, default= False)
+      created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+      def set_password(self, password: str):
+            self.password = bcrypt.hashpw(
+                  password.encode(),
+                  bcrypt.gensalt()
+            ).decode()
+
+      def check_password(self, password: str):
+            return bcrypt.checkpw(
+                  password.encode(),
+                  self.password.encode()
+            )
