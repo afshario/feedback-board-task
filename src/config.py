5aa13b2098ae5b92c6d17fb2fedd21fb6f0e2ad3
MHAfshari <mhafshari1385@gmail.com>
2026-06-11 13:45:02 +0330

    update(config): update configuration file for JWT

--- src/config.py
+++ src/config.py
@@ -8,5 +8,7 @@ PASS = os.getenv("DB_PASS")
 SERVER = os.getenv("DB_SERVER")
 PORT = os.getenv("DB_PORT")
 DB = os.getenv("DB")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
 
 DB_URL = f"postgresql://{USER}:{PASS}@{SERVER}:{PORT}/{DB}"
